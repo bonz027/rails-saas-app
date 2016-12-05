62477d8bb024d1400b3d1fe466908c5d9063e6b4
bonz027 <ocastro24@yahoo.com>
2016-12-05 01:49:56 +0000

    Added profiles create action

--- saasapp/app/controllers/profiles_controller.rb
+++ saasapp/app/controllers/profiles_controller.rb
@@ -1,8 +1,28 @@
 class ProfilesController < ApplicationController
   
-#GET to /users/:user_id/profile/new
+# GET to /users/:user_id/profile/new
   def new
-#Render blank profile details form
+# Render blank profile details form
   @profile = Profile.new
   end
-end
+# POST to /users/:user_id/profile 
+  def create
+# Ensure that we have the user who is filling out form
+   @user    = User.find( params[:user_id] )
+# Create profile linked to this specific user
+   @profile = @user.build_profile( profile_params) 
+# Saves profile to database   
+   if @profile.save
+     flash[:success] = "Profile updated!"
+     redirect_to root_path
+   else
+     render action: :new
+    end
+  end
+# Whitelist form fields for added security  
+  private 
+    def profile_params
+     params.require(:profile).permit(:first_name, :last_name, 
+     :job_title, :phone_number, :contact_email, :description)
+    end
+  end  
