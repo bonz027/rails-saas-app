a05df37953a4f17f13cb1c3079a81902145832e0
bonz027 <ocastro24@yahoo.com>
2016-12-05 22:19:24 +0000

    Added profile update action

--- saasapp/app/controllers/profiles_controller.rb
+++ saasapp/app/controllers/profiles_controller.rb
@@ -10,11 +10,11 @@ class ProfilesController < ApplicationController
 # Ensure that we have the user who is filling out form
    @user    = User.find( params[:user_id] )
 # Create profile linked to this specific user
-   @profile = @user.build_profile( profile_params) 
+   @profile = @user.build_profile(profile_params) 
 # Saves profile to database   
    if @profile.save
      flash[:success] = "Profile updated!"
-     redirect_to user_path( params[:user_id] )
+     redirect_to user_path(id: params[:user_id] )
    else
      render action: :new
     end
@@ -24,6 +24,22 @@ class ProfilesController < ApplicationController
   def edit
     @user    = User.find( params[:user_id] )
     @profile = @user.profile
+  end
+  
+# PUT to/users/:user_id/profile
+  def update
+# Retrieve the user from the database    
+    @user     = User.find( params[:user_id] )
+# Retrieve that user's profile    
+    @profile  = @user.profile
+# Mass assign edited profile attributes and save (update)    
+    if @profile.update_attributes(profile_params)
+      flash[:success] = "Profile updated!"
+      # Redirect user to their profile page
+      redirect_to user_path(id: params[:user_id] )
+    else
+      render action: :edit
+    end  
   end
   
 # Whitelist form fields for added security  
